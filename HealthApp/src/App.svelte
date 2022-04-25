fa2fb01c41cdca70895e4ffda2272f5e912d5ec6
Jack H <jackhalb@bu.edu>
2022-04-24 23:09:08 -0400

    created basic app
    added "hello world" level svelte app
    also added navigation between home page, login page and register page

---
+++ HealthApp/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	import router from "page"
+	import Home from '../components/Home.svelte'
+	import Login from '../components/Login.svelte'
+	import Register from '../components/Register.svelte'
+
+	let page;
+
+	router('/', () => page = Login)
+	router('/register', () => page = Register)
+	router('/home', () => page = Home)
+
+	router.start()
+</script>
+
+<svelte:component this={page} />
