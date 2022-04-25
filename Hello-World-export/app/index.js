9136d0fc559bf78f3f3a3c65f755cca0f6a534ee
jem2000 <justinem2000@gmail.com>
2022-04-25 01:00:55 -0400

    Added fitbit app

---
+++ Hello-World-export/app/index.js
@@ -0,0 +1,116 @@
+import { Accelerometer } from "accelerometer";
+import { Barometer } from "barometer";
+import { BodyPresenceSensor } from "body-presence";
+import { display } from "display";
+import * as document from "document";
+import { Gyroscope } from "gyroscope";
+import { HeartRateSensor } from "heart-rate";
+import { OrientationSensor } from "orientation";
+import * as document from "document";
+
+const accelLabel = document.getElementById("accel-label");
+const accelData = document.getElementById("accel-data");
+
+const barLabel = document.getElementById("bar-label");
+const barData = document.getElementById("bar-data");
+
+const bpsLabel = document.getElementById("bps-label");
+const bpsData = document.getElementById("bps-data");
+
+const gyroLabel = document.getElementById("gyro-label");
+const gyroData = document.getElementById("gyro-data");
+
+const hrmLabel = document.getElementById("hrm-label");
+const hrmData = document.getElementById("hrm-data");
+
+const orientationLabel = document.getElementById("orientation-label");
+const orientationData = document.getElementById("orientation-data");
+
+const sensors = [];
+
+const myButton = document.getElementById("button-1");
+
+let user = {
+    name: "Patient Zero",
+    password: "zero"
+}
+
+async function login() {
+  console.log("hi")
+  let res = await fetch("http://127.0.0.1:5000/authenticate", {
+    method: 'GET',
+    body: JSON.stringify(user)
+        }
+    );
+  let data = await res.json();
+  console.log("hello")
+  return data;
+}
+
+myButton.addEventListener("click", (evt) => {
+  console.log("CLICKED");
+  login().then(data => console.log("data is", data));
+})
+
+if (Accelerometer) {
+  const accel = new Accelerometer({ frequency: 1 });
+  accel.addEventListener("reading", () => {
+    accelData.text = JSON.stringify({
+      x: accel.x ? accel.x.toFixed(1) : 0,
+      y: accel.y ? accel.y.toFixed(1) : 0,
+      z: accel.z ? accel.z.toFixed(1) : 0
+    });
+  });
+  sensors.push(accel);
+  accel.start();
+} else {
+  accelLabel.style.display = "none";
+  accelData.style.display = "none";
+}
+
+if (Barometer) {
+  const barometer = new Barometer({ frequency: 1 });
+  barometer.addEventListener("reading", () => {
+    barData.text = JSON.stringify({
+      pressure: barometer.pressure ? parseInt(barometer.pressure) : 0
+    });
+  });
+  sensors.push(barometer);
+  barometer.start();
+} else {
+  barLabel.style.display = "none";
+  barData.style.display = "none";
+}
+
+if (BodyPresenceSensor) {
+  const bps = new BodyPresenceSensor();
+  bps.addEventListener("reading", () => {
+    bpsData.text = JSON.stringify({
+      presence: bps.present
+    })
+  });
+  sensors.push(bps);
+  bps.start();
+} else {
+  bpsLabel.style.display = "none";
+  bpsData.style.display = "none";
+}
+
+if (HeartRateSensor) {
+  const hrm = new HeartRateSensor({ frequency: 1 });
+  hrm.addEventListener("reading", () => {
+    hrmData.text = JSON.stringify({
+      heartRate: hrm.heartRate ? hrm.heartRate : 0
+    });
+  });
+  sensors.push(hrm);
+  hrm.start();
+} else {
+  hrmLabel.style.display = "none";
+  hrmData.style.display = "none";
+}
+
+display.addEventListener("change", () => {
+  // Automatically stop all sensors when the screen is off to conserve battery
+  display.on ? sensors.map(sensor => sensor.start()) : sensors.map(sensor => sensor.stop());
+});
