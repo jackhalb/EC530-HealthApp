016855d28128605c2d7dab9c10150744bc99e307
jem2000 <justinem2000@gmail.com>
2022-04-28 00:21:25 -0400

    Can now upload health records to the database

--- fitbit530/app/index.js
+++ fitbit530/app/index.js
@@ -10,46 +10,61 @@ import * as document from "document";
 import * as messaging from "messaging";
 
 
-const accelLabel = document.getElementById("accel-label");
-const accelData = document.getElementById("accel-data");
-
-const barLabel = document.getElementById("bar-label");
-const barData = document.getElementById("bar-data");
-
-const bpsLabel = document.getElementById("bps-label");
-const bpsData = document.getElementById("bps-data");
-
-const gyroLabel = document.getElementById("gyro-label");
-const gyroData = document.getElementById("gyro-data");
-
-const hrmLabel = document.getElementById("hrm-label");
-const hrmData = document.getElementById("hrm-data");
-
-const orientationLabel = document.getElementById("orientation-label");
-const orientationData = document.getElementById("orientation-data");
+// const accelLabel = document.getElementById("accel-label");
+// const accelData = document.getElementById("accel-data");
+//
+// const barLabel = document.getElementById("bar-label");
+// const barData = document.getElementById("bar-data");
+//
+// const bpsLabel = document.getElementById("bps-label");
+// const bpsData = document.getElementById("bps-data");
+//
+// const gyroLabel = document.getElementById("gyro-label");
+// const gyroData = document.getElementById("gyro-data");
+//
+// const hrmLabel = document.getElementById("hrm-label");
+// const hrmData = document.getElementById("hrm-data");
+//
+// const orientationLabel = document.getElementById("orientation-label");
+// const orientationData = document.getElementById("orientation-data");
 
 const sensors = [];
 
+if (HeartRateSensor) {
+    console.log("This device has a HeartRateSensor!");
+    const hrm = new HeartRateSensor();
+    hrm.addEventListener("reading", () => {
+        console.log(`Current heart rate: ${hrm.heartRate}`);
+    });
+    hrm.start();
+} else {
+    console.log("This device does NOT have a HeartRateSensor!");
+}
+
 const myButton = document.getElementById("button-1");
+const username_label = document.getElementById("username");
 myButton.text = "CONNECT";
 
 messaging.peerSocket.addEventListener("message", (evt) => {
     // console.error(`Connection error: ${err.code} - ${err.message}`);
-    let received = JSON.stringify(evt.data)
-    console.log(received)
-    if (received === "\"LOGGED IN\"") {
+    // let received = JSON.stringify(evt.data)
+    let received = evt.data
+    console.log("RECEIVED[0] IS: ", received[0])
+    console.log("RECEIVED[1] IS: ", received[1])
+    if (received[0] === "LOGGED IN") {
         myButton.text = "UPLOAD"
+        username_label.text = received[1]
+    } else if (received[0] === "UPLOADED") {
+        myButton.text = "SUCCESS"
     }
 })
 
 function sendMessage() {
-    // Sample data
-    // const data = {
-    //     title: 'My test data',
-    //     isTest: true,
-    //     records: [1, 2, 3, 4]
-    // }
-    const data = 'CONNECT'
+    let data = "None"
+    if (myButton.text === 'CONNECT')
+        data = ['CONNECT']
+    else if (myButton.text === 'UPLOAD')
+        data = ['UPLOAD', 75]
     if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
         // Send the data to peer as a message
         messaging.peerSocket.send(data);
@@ -60,64 +75,64 @@ myButton.addEventListener("click", (evt) => {
     console.log("CLICKED");
     sendMessage()
 })
-
-if (Accelerometer) {
-    const accel = new Accelerometer({ frequency: 1 });
-    accel.addEventListener("reading", () => {
-        accelData.text = JSON.stringify({
-            x: accel.x ? accel.x.toFixed(1) : 0,
-            y: accel.y ? accel.y.toFixed(1) : 0,
-            z: accel.z ? accel.z.toFixed(1) : 0
-        });
-    });
-    sensors.push(accel);
-    accel.start();
-} else {
-    accelLabel.style.display = "none";
-    accelData.style.display = "none";
-}
-
-if (Barometer) {
-    const barometer = new Barometer({ frequency: 1 });
-    barometer.addEventListener("reading", () => {
-        barData.text = JSON.stringify({
-            pressure: barometer.pressure ? parseInt(barometer.pressure) : 0
-        });
-    });
-    sensors.push(barometer);
-    barometer.start();
-} else {
-    barLabel.style.display = "none";
-    barData.style.display = "none";
-}
-
-if (BodyPresenceSensor) {
-    const bps = new BodyPresenceSensor();
-    bps.addEventListener("reading", () => {
-        bpsData.text = JSON.stringify({
-            presence: bps.present
-        })
-    });
-    sensors.push(bps);
-    bps.start();
-} else {
-    bpsLabel.style.display = "none";
-    bpsData.style.display = "none";
-}
-
-if (HeartRateSensor) {
-    const hrm = new HeartRateSensor({ frequency: 1 });
-    hrm.addEventListener("reading", () => {
-        hrmData.text = JSON.stringify({
-            heartRate: hrm.heartRate ? hrm.heartRate : 0
-        });
-    });
-    sensors.push(hrm);
-    hrm.start();
-} else {
-    hrmLabel.style.display = "none";
-    hrmData.style.display = "none";
-}
+//
+// if (Accelerometer) {
+//     const accel = new Accelerometer({ frequency: 1 });
+//     accel.addEventListener("reading", () => {
+//         accelData.text = JSON.stringify({
+//             x: accel.x ? accel.x.toFixed(1) : 0,
+//             y: accel.y ? accel.y.toFixed(1) : 0,
+//             z: accel.z ? accel.z.toFixed(1) : 0
+//         });
+//     });
+//     sensors.push(accel);
+//     accel.start();
+// } else {
+//     accelLabel.style.display = "none";
+//     accelData.style.display = "none";
+// }
+//
+// if (Barometer) {
+//     const barometer = new Barometer({ frequency: 1 });
+//     barometer.addEventListener("reading", () => {
+//         barData.text = JSON.stringify({
+//             pressure: barometer.pressure ? parseInt(barometer.pressure) : 0
+//         });
+//     });
+//     sensors.push(barometer);
+//     barometer.start();
+// } else {
+//     barLabel.style.display = "none";
+//     barData.style.display = "none";
+// }
+//
+// if (BodyPresenceSensor) {
+//     const bps = new BodyPresenceSensor();
+//     bps.addEventListener("reading", () => {
+//         bpsData.text = JSON.stringify({
+//             presence: bps.present
+//         })
+//     });
+//     sensors.push(bps);
+//     bps.start();
+// } else {
+//     bpsLabel.style.display = "none";
+//     bpsData.style.display = "none";
+// }
+//
+// if (HeartRateSensor) {
+//     const hrm = new HeartRateSensor({ frequency: 1 });
+//     hrm.addEventListener("reading", () => {
+//         hrmData.text = JSON.stringify({
+//             heartRate: hrm.heartRate ? hrm.heartRate : 0
+//         });
+//     });
+//     sensors.push(hrm);
+//     hrm.start();
+// } else {
+//     hrmLabel.style.display = "none";
+//     hrmData.style.display = "none";
+// }
 
 display.addEventListener("change", () => {
     // Automatically stop all sensors when the screen is off to conserve battery
--- fitbit530/companion/index.js
+++ fitbit530/companion/index.js
@@ -32,17 +32,26 @@ function sendMessage(data) {
 }
 
 messaging.peerSocket.addEventListener("message", (evt) => {
-    let received = JSON.stringify(evt.data)
-    if (received === "\"CONNECT\"") {
+    // let received = JSON.stringify(evt.data)
+    let received = evt.data
+    if (received[0] === "CONNECT") {
         login().then(r => {
             console.log(r)
             if (r[1] === 200) {
-                sendMessage("LOGGED IN")
+                sendMessage(["LOGGED IN", JSON.parse(settingsStorage.getItem("username")).name])
             }
             else {
                 console.log(r[1])
             }
 
+        })
+    }
+    else if (received[0] === "UPLOAD") {
+        post_reading(received[1]).then(r => {
+            console.log(r)
+            if (r[1] === 200) {
+                sendMessage(["UPLOADED"])
+            }
         })
     }
     else {
@@ -52,14 +61,15 @@ messaging.peerSocket.addEventListener("message", (evt) => {
 
 console.log(typeof settingsStorage.getItem("username"))
 
-const API = 'http://127.0.0.1:5000/authenticate'
+const auth = 'http://127.0.0.1:5000/authenticate'
+const create_reading = 'http://127.0.0.1:5000/new-reading'
 
 async function login() {
     let user = {
         name: JSON.parse(settingsStorage.getItem("username")).name,
         password: JSON.parse(settingsStorage.getItem("password")).name
     }
-    let res = await fetch(API, {
+    let res = await fetch(auth, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -69,4 +79,23 @@ async function login() {
     );
     let data = await res.json();
     return [data, res.status];
+}
+
+async function post_reading(data) {
+    let health_reading = {
+        "heart_rate": data
+    }
+    let health_item = {
+        name: JSON.parse(settingsStorage.getItem("username")).name,
+        health_reading: health_reading
+    }
+    let res = await fetch(create_reading, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(health_item)
+        }
+    );
+    return [await res.json(), res.status]
 }
