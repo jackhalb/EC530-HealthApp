6591a21530b9cd5bae91f364303abd37241d4db4
Jack H <jackhalb@bu.edu>
2022-05-08 16:47:01 -0400

    Update Home.svelte
    pretty list for users health records

--- HealthApp/components/Home.svelte
+++ HealthApp/components/Home.svelte
@@ -2,11 +2,13 @@
 	export let params
 	import { onMount } from 'svelte'
 
-	let data = []
+	let data;
+	let healthRecords = [];
+	let cred;
 
-	onMount(async () => {
-		let cred = params.data
-		let name = {
+	let promise = onMount(async () => {
+		cred = params.data
+		let username = {
         	username: cred
     	};
 		let user_data = await fetch('http://127.0.0.1:5000/find-user', {
@@ -14,20 +16,28 @@
             	headers: {
             	    'Content-Type': 'application/json'
             	},
-            	body: JSON.stringify(name)
+            	body: JSON.stringify(username)
         	});
 		data = await user_data.json()
-		console.log(data)
+		healthRecords = data['health_records']
 	});
-
-	// console.log(data)
-
-    
 </script>
 
 <main>
-	<h1>Hello user!</h1>
-	<p><a href="http://localhost:8080/">Logout</a></p>
+	<h1>Hello {cred}!</h1>
+	{#await promise}
+		<p>waiting...</p>
+	{:then}
+		{#each healthRecords as healthRecord}
+		<h2>{healthRecord.reading_date}</h2>
+		{#each Object.entries(healthRecord) as [key, value]}
+			<p>
+				{key}: {value}
+			</p>
+		{/each}
+		{/each}
+	{/await}
+	<h2><a href="http://localhost:8080/">Logout</a></h2>
 </main>
 
 <style>
